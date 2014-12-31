e84a5e8b02ec7d78206b0054886a433f64af16c3
Logan Bell <loganbell@gmail.com>
2014-12-30 16:43:30 -0800

    Adding a way to setup some sort of configuraiton

--- proxy.go
+++ proxy.go
@@ -4,18 +4,38 @@ import (
 	"net/http"
 	"html"
 	"fmt"
+	"encoding/json"
+	"os"
 )
+
+type MapType struct {
+	Incoming_Url string
+	Url_Pool []string
+}
+
+type Configuration struct {
+	Maps []MapType
+}
 
 func main() {
 	var port = 3000
 
-	fmt.Println("let the party begin at %d", port)
+	fmt.Println("let the party begin at: ", port)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w,"Woot! %q", html.EscapeString(r.URL.Path))
 	})
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	file, _ := os.Open("./conf.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	fmt.Printf("Configuration: %v\n", configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
 	if err != nil {
 		fmt.Println("Ah shoot johny: %v", err)
